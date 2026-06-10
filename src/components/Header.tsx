49ad321346475f114db5f8322bc510216dd53d4b
ljh200048 <lch200048@gmail.com>
2026-06-10 09:38:13 +0900

    Update header text from '청춘필름' to 'CHEONGCHUN FILM'

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -46,7 +46,7 @@ export default function Header({
           >
             <Film className="w-5 h-5 text-amber-500 transition-transform duration-500 group-hover:rotate-12" />
             <span className="font-display text-lg font-bold tracking-widest text-[#fafaf9] group-hover:text-amber-400 transition-colors">
-              청춘필름
+              CHEONGCHUN FILM
             </span>
           </div>
 
